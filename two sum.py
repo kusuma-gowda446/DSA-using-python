01e9695052851fa4b56c3b5a2bbb7388ecb4bdb2
Kusuma.B.J <kusumabj040406@gmail.com>
2026-02-20 20:57:30 +0530

    Implement twoSum function to find indices
    Fix syntax errors in twoSum function.

---
+++ two sum.py
@@ -0,0 +1,13 @@
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n=len(nums)
+        for i in range(n-1):
+            for j in range(j+1,n-1)
+              if nums[i]+nums[j]=target
+                  return[i,j]
+
+        
