18fc7754d71b53d29c383d953a0b5cb94ce3bd28
Kusuma.B.J <kusumabj040406@gmail.com>
2026-02-28 20:37:11 +0530

    Add productExceptSelf function
    Implement product of array except self algorithm.

---
+++ product_of_array_except_self.py
@@ -0,0 +1,18 @@
+def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        result=[1]*n
+        left=1
+        for i in range(n):
+            result[i]=left
+            left*=nums[i]
+        right=1
+        for i in range(n-1,-1,-1):
+            result[i]*=right
+            right*=nums[i]
+        return result
+
+        
