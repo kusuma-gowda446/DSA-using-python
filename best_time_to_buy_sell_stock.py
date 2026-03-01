a352fdd5c4ff171decc7ed7ccfda2e75489ebc70
Kusuma.B.J <kusumabj040406@gmail.com>
2026-03-01 20:51:08 +0530

    Implement maxProfit function to calculate stock profit

---
+++ best_time_to_buy_sell_stock.py
@@ -0,0 +1,13 @@
+def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_profit=float("inf")
+        max_profit=0
+        for price in prices:
+            if price<min_profit:
+                min_profit=price
+            elif price-min_profit>max_profit:
+                max_profit=price-min_profit
+        return max_profit
