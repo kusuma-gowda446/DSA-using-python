583e254673ca90befd9c545d409b0e1af857f3b5
Kusuma.B.J <kusumabj040406@gmail.com>
2026-04-12 14:42:07 +0530

    Implement isHappy function to check happy numbers

---
+++ happy_number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen=set()
+        while n!=1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum(int(d)**2 for d in str(n) )
+        return True
+
+        
