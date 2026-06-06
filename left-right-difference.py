f5d82706501846c3cb9e9612449f242d6a2868f3
Kusuma.B.J <kusumabj040406@gmail.com>
2026-06-06 21:00:52 +0530

    Implement left-right difference calculation

---
+++ left-right-difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def leftRightDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        total = sum(nums)
+        left = 0
+        ans = []
+
+        for num in nums:
+            right = total - left - num
+            ans.append(abs(left - right))
+            left += num
+
+        return ans
+        
