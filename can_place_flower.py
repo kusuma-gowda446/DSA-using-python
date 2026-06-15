713bc19e2f649137d83404893c9b4b5ff751fa6d
Kusuma.B.J <kusumabj040406@gmail.com>
2026-06-15 19:21:44 +0530

    Implement canPlaceFlowers method in Solution class

---
+++ can_place_flower.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        count = 0
+
+        for i in range(len(flowerbed)):
+            if (flowerbed[i] == 0 and
+                (i == 0 or flowerbed[i - 1] == 0) and
+                (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+                
+                flowerbed[i] = 1
+                count += 1
+
+                if count >= n:
+                    return True
+
+        return count >= n
+        
