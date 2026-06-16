4f55fe9a05a154e5a0f6b58a41e2011814a075a8
Kusuma.B.J <kusumabj040406@gmail.com>
2026-06-16 21:03:00 +0530

    Implement string processing with special operations

---
+++ processing_string_with_special_operations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def processStr(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ""
+
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                res += ch
+            elif ch == '*':
+                if res:
+                    res = res[:-1]
+            elif ch == '#':
+                res += res
+            elif ch == '%':
+                res = res[::-1]
+
+        return res
+        
