91e9f556c4d5e7d9c17f5dc246f43e242304a05c
Kusuma.B.J <kusumabj040406@gmail.com>
2026-06-20 08:43:38 +0530

    Implement maxBuilding function to calculate max height

---
+++ maximum_building_heaight.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def maxBuilding(self, n, restrictions):
+        """
+        :type n: int
+        :type restrictions: List[List[int]]
+        :rtype: int
+        """
+        restrictions.append([1, 0])
+
+        found = False
+        for r in restrictions:
+            if r[0] == n:
+                r[1] = min(r[1], n - 1)
+                found = True
+
+        if not found:
+            restrictions.append([n, n - 1])
+
+        restrictions.sort()
+
+        m = len(restrictions)
+
+       
+        for i in range(1, m):
+            d = restrictions[i][0] - restrictions[i - 1][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i - 1][1] + d
+            )
+
+        
+        for i in range(m - 2, -1, -1):
+            d = restrictions[i + 1][0] - restrictions[i][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i + 1][1] + d
+            )
+
+        ans = 0
+
+        for i in range(1, m):
+            x1, h1 = restrictions[i - 1]
+            x2, h2 = restrictions[i]
+
+            d = x2 - x1
+            ans = max(ans, (h1 + h2 + d) // 2)
+
+        return ans
+        
