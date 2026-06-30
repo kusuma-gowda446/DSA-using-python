e2c6f063ecb348580650213618f9d1458ff2c042
Kusuma.B.J <kusumabj040406@gmail.com>
2026-06-30 09:32:39 +0530

    Create number_of_substring_containing_all_3_characters.py

---
+++ number_of_substring_containing_all_3_characters.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = {'a': 0, 'b': 0, 'c': 0}
+        left = 0
+        ans = 0
+        n = len(s)
+
+        for right in range(n):
+            count[s[right]] += 1
+
+            while count['a'] > 0 and count['b'] > 0 and count['c'] > 0:
+                ans += n - right
+                count[s[left]] -= 1
+                left += 1
+
+        return ans
+        
